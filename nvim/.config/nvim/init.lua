b1797b22bdf478ca5cb5d8852e0485337fdacab9
Nelson R Monserrate <nrmonserrate@gmail.com>
2024-05-16 13:25:58 -0400

    Add packer and colorscheme

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,25 @@
+-- Bootstrap packer if not installed
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd 'packadd packer.nvim'
+end
+
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- Simple plugins can be specified as strings
+  use "ellisonleao/gruvbox.nvim" 
+end)
+
+
